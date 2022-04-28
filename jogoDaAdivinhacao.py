55d833d48e5e8773b3d9aaf4909912f289055891
Thiago Callegari <92542640+thiagodines@users.noreply.github.com>
2022-04-28 15:59:31 -0300

    Add files via upload

---
+++ jogoDaAdivinhacao.py
@@ -0,0 +1,44 @@
+secreto = input('Digite uma palavra secreta: ')
+digitadas = []
+chances = 3
+
+
+while True:
+    if chances <= 0:
+        print('Você perdeu')
+        break
+
+    letra = input('Digite uma letra: ')
+
+    if len(letra) > 1:
+        print('Ahhh isso não vale, digite apenas uma letra.')
+        continue
+
+    digitadas.append(letra)
+
+    if letra in secreto:
+        print(f'Parabéns, a letra "{letra.upper()}" existe na palavra secreta.')
+    else:
+        print(f'EEERROOUUU!!. A letra "{letra.upper()}" NÃO EXISTE na palavra secreta.')
+        digitadas.pop()
+
+
+    secreto_temporario = ''
+    for letra_secreta in secreto:
+        if letra_secreta in digitadas:
+            secreto_temporario += letra_secreta
+        else:
+            secreto_temporario += '*'
+
+    if secreto_temporario == secreto:
+        print(f'PARABÉNS! VOCÊ GANHOU!!!!. A palavra era {secreto_temporario}.')
+        break
+    else:
+        print(f'A palavra secreta está assim: {secreto_temporario}')
+
+    if letra not in secreto:
+        chances -= 1
+
+    print(f'Você ainda tem {chances} chances.')
+    print()
+
